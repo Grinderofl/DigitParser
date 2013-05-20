865c1b2b5fef11814a09ad98f1810740558a67be
Nero Sule <sulenero@gmail.com>
2013-05-20 16:53:20 +0300

    Initial commit

---
+++ DigitParser/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitParser
+{
+    public partial class Form1 : Form
+    {
+        private readonly BackgroundWorker _worker;
+        private FileDialog _dialog;
+        private bool _cancel = false;
+        public Form1()
+        {
+            InitializeComponent();
+            _worker = new BackgroundWorker {WorkerReportsProgress = true};
+            _worker.DoWork += WorkerOnDoWork;
+            _worker.ProgressChanged += WorkerOnProgressChanged;
+            _worker.RunWorkerCompleted += WorkerOnRunWorkerCompleted;
+        }
+
+        private void WorkerOnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs runWorkerCompletedEventArgs)
+        {
+            progressBar1.Value = 0;
+        }
+
+        private void WorkerOnProgressChanged(object sender, ProgressChangedEventArgs progressChangedEventArgs)
+        {
+            progressBar1.Value = progressChangedEventArgs.ProgressPercentage;//progressChangedEventArgs.ProgressPercentage;
+        }
+
+        private void WorkerOnDoWork(object sender, DoWorkEventArgs doWorkEventArgs)
+        {
+            var fi = new FileInfo(textBox1.Text);
+            var size = fi.Length;
+            var orig = size;
+            var distance = (int)(size/100.0f);
+            var current = 0;
+            var val = "";
+            using(var fs = File.OpenText(textBox1.Text))
+            {
+                using(var ws = File.OpenWrite(textBox2.Text))
+                {
+                    while (!fs.EndOfStream)
+                    {
+                        current++;
+                        size--;
+                        if (current > distance)
+                        {
+                            _worker.ReportProgress((int) (size/orig*100));
+                            current = 0;
+                        }
+                        var old = val;
+                        var f = (char) fs.Read();
+                        int s;
+                        if (!int.TryParse(f.ToString(), out s)) continue;
+
+                        val += f;
+                        if (int.Parse(val) <= 255 && val.Length <= 3) continue;
+                        var b = Byte.Parse(old);
+                        if (checkBox1.Checked)
+                        {
+                            if (b == 32 || (b >= 48 && b <= 57) || (b >= 97 && b <= 122) || (b >= 65 && b <= 90))
+                                ws.WriteByte(b);
+                            
+                        }
+                        else
+                        {
+                            ws.WriteByte(b);
+                            
+                        }
+                        val = f.ToString();
+                    }
+                }
+            }
+        }
+
+        private void Button3Click(object sender, EventArgs e)
+        {
+            _cancel = false;
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+                Error("Input or output missing");
+
+            if(!File.Exists(textBox1.Text))
+                Error("Input file is invalid or missing");
+
+            FileStream fs = null;
+            try
+            {
+                fs = File.Create(textBox2.Text);
+            }
+            catch (Exception)
+            {
+                Error("Output file could not be created");
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                    fs.Dispose();
+                }
+            }
+
+            if (_cancel)
+                return;
+
+            _worker.RunWorkerAsync();
+
+        }
+
+        private void Error(string message)
+        {
+            _cancel = true;
+            MessageBox.Show(message);
+        }
+
+        private void Button1Click(object sender, EventArgs e)
+        {
+            _dialog = new OpenFileDialog();
+            if(_dialog.ShowDialog() == DialogResult.OK)
+                textBox1.Text = _dialog.FileName;
+        }
+
+        private void Button2Click(object sender, EventArgs e)
+        {
+            _dialog = new SaveFileDialog();
+            if (_dialog.ShowDialog() == DialogResult.OK)
+                textBox2.Text = _dialog.FileName;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+Program to parse all numeric digits in a text file and convert their values into ASCII characters, based on
+
+http://stackoverflow.com/questions/12991606/how-to-take-in-digits-0-255-from-a-file-with-no-delimeters
+
+This program is used as a proof of concept base for the theory of "everything is contained within the digits of Pi".
+
+Improvements should be made in future to allow a different set of parsing, such as reading numbers contiguously rather than until 255 is passed.
